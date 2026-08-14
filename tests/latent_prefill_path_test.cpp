a6a4445621a946bc8175249706c61ede70e8226b
celsowm <celsowm@gmail.com>
2026-08-14 20:44:55 -0300

    Add latent prefill routing regression test

---
+++ tests/latent_prefill_path_test.cpp
@@ -0,0 +1,31 @@
+#include "latent_path.hpp"
+
+#include <stdexcept>
+
+int main() {
+    celeg::AttentionSpec factorized;
+    celeg::LatentAttentionStateSpec factorized_state;
+    factorized_state.factorized = true;
+    factorized.state = factorized_state;
+    if (celeg::prefill_detail::latent_prefill_path(factorized) !=
+        celeg::prefill_detail::LatentPrefillPath::Factorized) {
+        return 1;
+    }
+
+    celeg::AttentionSpec projected;
+    celeg::LatentAttentionStateSpec projected_state;
+    projected_state.factorized = false;
+    projected.state = projected_state;
+    if (celeg::prefill_detail::latent_prefill_path(projected) !=
+        celeg::prefill_detail::LatentPrefillPath::Projected) {
+        return 2;
+    }
+
+    celeg::AttentionSpec ordinary;
+    try {
+        (void)celeg::prefill_detail::latent_prefill_path(ordinary);
+    } catch (const std::logic_error&) {
+        return 0;
+    }
+    return 3;
+}
