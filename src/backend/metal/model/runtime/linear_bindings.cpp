4ecedb4609a605364008cdce20ee25945d6264db
celsowm <celsowm@gmail.com>
2026-08-30 08:03:13 -0300

    Remove duplicated Metal GGML block sizes

--- src/backend/metal/model/runtime/linear_bindings.cpp
+++ src/backend/metal/model/runtime/linear_bindings.cpp
@@ -4,11 +4,11 @@ namespace celeg::metal_model_detail {
 namespace {
 
 constexpr MetalLinearBinding kBindings[] = {
-    {GgmlType::Q4_0, MetalLinearStorage::Q4_0, 32},
-    {GgmlType::Q4_K, MetalLinearStorage::Q4K, 256},
-    {GgmlType::Q5_K, MetalLinearStorage::Q5K, 256},
-    {GgmlType::Q6_K, MetalLinearStorage::Q6K, 256},
-    {GgmlType::Q8_0, MetalLinearStorage::Q8_0, 32},
+    {GgmlType::Q4_0, MetalLinearStorage::Q4_0},
+    {GgmlType::Q4_K, MetalLinearStorage::Q4K},
+    {GgmlType::Q5_K, MetalLinearStorage::Q5K},
+    {GgmlType::Q6_K, MetalLinearStorage::Q6K},
+    {GgmlType::Q8_0, MetalLinearStorage::Q8_0},
 };
 
 }
