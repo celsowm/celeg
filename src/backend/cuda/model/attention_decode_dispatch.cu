cd7df9885e4f0ce9d995772c63722554d192d8c1
celsowm <celsowm@gmail.com>
2026-08-31 08:22:27 -0300

    Own CUDA contiguous decode attention dispatch

---
+++ src/backend/cuda/model/attention_decode_dispatch.cu
@@ -0,0 +1,154 @@
+#include "attention_decode_dispatch.hpp"
+
+#include <stdexcept>
+
+namespace celeg {
+
+GqaGeometry make_cuda_gqa_geometry(
+    const AttentionSpec& layout,
+    const AttentionSpec& owner_layout) {
+    return {
+        .q_heads = layout.query_heads,
+        .kv_heads = owner_layout.key_value_heads,
+        .head_dim = owner_layout.head_dim,
+        .sliding_window = layout.sliding_window_size()};
+}
+
+GqaBlockSparsePattern lower_cuda_block_sparse_pattern(
+    const BlockSparsePattern& pattern) {
+    return {
+        .block_size = pattern.block_size,
+        .local_blocks = pattern.local_blocks,
+        .global_blocks = pattern.global_blocks};
+}
+
+void dispatch_cuda_contiguous_decode_attention(
+    const CudaContiguousDecodeDispatch& dispatch) {
+    const bool int8_kv = dispatch.plan.kv_format == KvCacheMode::Int8;
+    const bool device_position =
+        dispatch.position_mode == CudaDecodePositionMode::DeviceCounter;
+
+    switch (dispatch.plan.algorithm) {
+    case AttentionAlgorithm::Alibi:
+        if (!device_position) throw UnsupportedAttentionCapability(dispatch.plan);
+        if (int8_kv) {
+            launch_gqa_decode_alibi_int8_device({
+                .query = dispatch.query,
+                .kv = dispatch.int8_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .alibi_slopes = dispatch.alibi_slopes,
+                .stream = dispatch.stream});
+        } else {
+            launch_gqa_decode_alibi_device({
+                .query = dispatch.query,
+                .kv = dispatch.bf16_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .alibi_slopes = dispatch.alibi_slopes,
+                .stream = dispatch.stream});
+        }
+        return;
+
+    case AttentionAlgorithm::Segmented:
+        if (!device_position) throw UnsupportedAttentionCapability(dispatch.plan);
+        if (int8_kv) {
+            launch_gqa_decode_segmented_int8_device({
+                .query = dispatch.query,
+                .kv = dispatch.int8_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .segmentation = dispatch.segmentation,
+                .stream = dispatch.stream});
+        } else {
+            launch_gqa_decode_segmented_device({
+                .query = dispatch.query,
+                .kv = dispatch.bf16_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .segmentation = dispatch.segmentation,
+                .stream = dispatch.stream});
+        }
+        return;
+
+    case AttentionAlgorithm::Online:
+        if (int8_kv) {
+            const GqaContiguousInt8Args args{
+                .query = dispatch.query,
+                .kv = dispatch.int8_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .stream = dispatch.stream};
+            if (device_position) launch_gqa_decode_online_int8_device(args);
+            else launch_gqa_decode_online_int8(args);
+        } else {
+            const GqaContiguousArgs args{
+                .query = dispatch.query,
+                .kv = dispatch.bf16_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .stream = dispatch.stream};
+            if (device_position) launch_gqa_decode_online_device(args);
+            else launch_gqa_decode_online(args);
+        }
+        return;
+
+    case AttentionAlgorithm::Strict:
+        if (dispatch.block_sparse) {
+            const GqaBlockSparsePattern pattern =
+                lower_cuda_block_sparse_pattern(*dispatch.block_sparse);
+            if (int8_kv) {
+                launch_gqa_decode_block_sparse_int8_device({
+                    .query = dispatch.query,
+                    .kv = dispatch.int8_kv,
+                    .out = dispatch.out,
+                    .geometry = dispatch.geometry,
+                    .extent = dispatch.extent,
+                    .stream = dispatch.stream}, pattern);
+            } else {
+                launch_gqa_decode_block_sparse_device({
+                    .query = dispatch.query,
+                    .kv = dispatch.bf16_kv,
+                    .out = dispatch.out,
+                    .geometry = dispatch.geometry,
+                    .extent = dispatch.extent,
+                    .stream = dispatch.stream}, pattern);
+            }
+            return;
+        }
+        if (int8_kv) {
+            const GqaContiguousInt8Args args{
+                .query = dispatch.query,
+                .kv = dispatch.int8_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .stream = dispatch.stream};
+            if (device_position) launch_gqa_decode_strict_int8_device(args);
+            else launch_gqa_decode_strict_int8(args);
+        } else {
+            const GqaContiguousArgs args{
+                .query = dispatch.query,
+                .kv = dispatch.bf16_kv,
+                .out = dispatch.out,
+                .geometry = dispatch.geometry,
+                .extent = dispatch.extent,
+                .stream = dispatch.stream};
+            if (device_position) launch_gqa_decode_strict_device(args);
+            else launch_gqa_decode_strict(args);
+        }
+        return;
+
+    case AttentionAlgorithm::Flash:
+    case AttentionAlgorithm::Gemm:
+        throw UnsupportedAttentionCapability(dispatch.plan);
+    }
+}
+
+}
