b68fe035e800ce428b1f835d88e832e0c1b9f142
celsowm <celsowm@gmail.com>
2026-08-31 08:41:42 -0300

    Centralize CUDA QKV projection layout

--- src/backend/cuda/model/attention_layer_support.hpp
+++ src/backend/cuda/model/attention_layer_support.hpp
@@ -12,10 +12,21 @@ struct CudaAttentionOwner {
     int model_layer = -1;
 };
 
+struct CudaQkvProjectionView {
+    __nv_bfloat16* query = nullptr;
+    __nv_bfloat16* key = nullptr;
+    __nv_bfloat16* value = nullptr;
+    int query_projection_width = 0;
+};
+
 CudaAttentionOwner resolve_cuda_attention_owner(
     AttentionLayer& attention,
     int layer_index,
     std::vector<Layer>& layers);
+
+CudaQkvProjectionView make_cuda_qkv_projection_view(
+    AttentionLayer& attention,
+    __nv_bfloat16* storage);
 
 Bf16KvView cuda_bf16_kv_view(AttentionLayer& owner);
 Int8KvView cuda_int8_kv_view(AttentionLayer& owner);
