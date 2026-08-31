b83a720b88d9495903021bd76f8b42bfedcf108c
celsowm <celsowm@gmail.com>
2026-08-31 18:48:37 -0300

    Test backend attention capability contracts

---
+++ tests/model/attention_backend_capabilities_test.cpp
@@ -0,0 +1,70 @@
+#include "celeg/backend/metal/attention_capabilities.hpp"
+#include "support/assertions.hpp"
+
+#include <stdexcept>
+
+namespace {
+
+celeg::CompiledModelProgram program_with(celeg::AttentionSpec attention) {
+    celeg::CompiledAttentionProgram compiled;
+    compiled.semantics = std::move(attention);
+    compiled.execution.kind = celeg::AttentionExecutionKind::Standard;
+    celeg::CompiledLayerProgram layer;
+    layer.mixer = std::move(compiled);
+    celeg::CompiledModelProgram program;
+    program.layers.push_back(std::move(layer));
+    return program;
+}
+
+template <typename Mutator>
+bool metal_rejects(Mutator mutate) {
+    celeg::AttentionSpec attention;
+    attention.query_heads = 1;
+    attention.key_value_heads = 1;
+    attention.head_dim = 8;
+    mutate(attention);
+    try {
+        celeg::validate_metal_attention_capabilities(program_with(std::move(attention)));
+    } catch (const std::invalid_argument&) {
+        return true;
+    }
+    return false;
+}
+
+}
+
+int main() {
+    CELEG_TEST_CHECK(!metal_rejects([](auto&) {}));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.pattern = celeg::SlidingWindowPattern{128};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.pattern = celeg::BidirectionalPattern{};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.pattern = celeg::PrefixLmPattern{4};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.pattern = celeg::BlockSparsePattern{16, 2, 1};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.pattern = celeg::DynamicSparsePattern{16, 4};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.key_value_source = celeg::ExternalMemorySource{0};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.bias = celeg::AlibiBiasSpec{{1.0f}};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.bias = celeg::RelativePositionBiasSpec{32, 128, false};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.position = celeg::NoPositionEncodingSpec{};
+    }));
+    CELEG_TEST_CHECK(metal_rejects([](auto& attention) {
+        attention.position = celeg::MultiAxisRopeSpec{};
+    }));
+
+    return 0;
+}
