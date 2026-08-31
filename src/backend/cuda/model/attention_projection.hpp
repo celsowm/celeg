ac99f882fce4d041bae0570e28773cf35523ef64
celsowm <celsowm@gmail.com>
2026-08-31 09:44:04 -0300

    Own CUDA projected latent binding validation

--- src/backend/cuda/model/attention_projection.hpp
+++ src/backend/cuda/model/attention_projection.hpp
@@ -5,6 +5,8 @@ namespace celeg {
 struct AttentionLayer;
 struct CudaCompiledModel;
 
+void require_cuda_projected_latent_bindings(const AttentionLayer& attention);
+
 void project_cuda_latent_attention_qkv(
     CudaCompiledModel& model, AttentionLayer& attention);
 
