018ed10ee9da39664992c53d8e949a6e62483ed2
celsowm <celsowm@gmail.com>
2026-08-31 08:37:00 -0300

    Add CUDA attention layer owner contract

---
+++ src/backend/cuda/model/attention_layer_support.hpp
@@ -0,0 +1,22 @@
+#pragma once
+
+#include "detail/layer_state.hpp"
+
+#include <vector>
+
+namespace celeg {
+
+struct CudaAttentionOwner {
+    AttentionLayer* layer = nullptr;
+    int model_layer = -1;
+};
+
+CudaAttentionOwner resolve_cuda_attention_owner(
+    AttentionLayer& attention,
+    int layer_index,
+    std::vector<Layer>& layers);
+
+Bf16KvView cuda_bf16_kv_view(AttentionLayer& owner);
+Int8KvView cuda_int8_kv_view(AttentionLayer& owner);
+
+}
