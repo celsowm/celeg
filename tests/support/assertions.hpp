328cba13607a383eb12ce634c2b7592e4aa6fd82
celsowm <celsowm@gmail.com>
2026-09-08 15:39:10 -0300

    test: make environment helpers portable on Windows

--- tests/support/assertions.hpp
+++ tests/support/assertions.hpp
@@ -1,8 +1,20 @@
 #pragma once
 
+#include <cstdlib>
+#include <cstdio>
 #include <stdexcept>
 #include <string>
-#include <cstdio>
+
+#if defined(_WIN32)
+inline int setenv(const char* name, const char* value, int overwrite) {
+    if (!overwrite && std::getenv(name) != nullptr) return 0;
+    return ::_putenv_s(name, value);
+}
+
+inline int unsetenv(const char* name) {
+    return ::_putenv_s(name, "");
+}
+#endif
 
 namespace celeg::test {
 
