d63f1aa4c3b01614210e8fc95c8920f73af34126
celsowm <celsowm@gmail.com>
2026-09-09 22:48:04 -0300

    metal: advertise value normalization support

--- src/celeg/backend/metal/attention_capabilities.hpp
+++ src/celeg/backend/metal/attention_capabilities.hpp
@@ -24,7 +24,7 @@ inline constexpr AttentionBackendCapabilities metal_attention_capabilities() {
         .standard_execution = true,
         .latent_execution = false,
         .factorized_latent_execution = false,
-        .value_norm = false,
+        .value_norm = true,
     };
 }
 
